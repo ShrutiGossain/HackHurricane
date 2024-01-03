4798f13de125604ff55ad4afe1dfd30646aa18ab
vaishali <vaishalirdj14@gmail.com>
2024-01-03 23:26:38 +0530

    routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Github from "./components/Github";
+import Leetcode from "./components/Leetcode";
+import Quotes from "./components/Quotes";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import UserDashboard from "./pages/UserDashboard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/user" element={<UserDashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
