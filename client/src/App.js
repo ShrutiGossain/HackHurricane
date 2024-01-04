88e52bf66363ae0e763c87ea6ddc5db1c4c073b6
ShrutiGossain <shrutigossain335@gmail.com>
2024-01-04 15:52:18 +0530

    links added

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,18 @@ import Quotes from "./components/Quotes";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import UserDashboard from "./pages/UserDashboard";
+import Links from "./components/Links";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signup />}></Route>
+        {/* <Route path="/" element={<Signup />}></Route> */}
         <Route path="/login" element={<Login />}></Route>
         <Route path="/user" element={<UserDashboard />}></Route>
+        <Route path="/" element={<Links />}></Route>
+
       </Routes>
     </BrowserRouter>
   );
