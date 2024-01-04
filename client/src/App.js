9e0af2397c4bad3617c731a182628e39878315f9
Shreya Choudhary <shreyachoudhary2019@gmail.com>
2024-01-04 14:17:37 +0530

    chhh

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Quotes from "./components/Quotes";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import UserDashboard from "./pages/UserDashboard";
+import Sidebar from "./components/Sidebar.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/user" element={<UserDashboard />}></Route>
+        <Route path="/Sidebar" element={<Sidebar/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,143 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+      {/* Container wrapper */}
+      <div className="container-fluid">
+        {/* Toggle button */}
+        <button
+          data-mdb-collapse-init
+          className="navbar-toggler"
+          type="button"
+          data-mdb-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fas fa-bars"></i>
+        </button>
 
-export default Navbar
+        {/* Collapsible wrapper */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          {/* Navbar brand */}
+          <a className="navbar-brand mt-2 mt-lg-0" href="#">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+              height="15"
+              alt="MDB Logo"
+              loading="lazy"
+            />
+          </a>
+          {/* Left links */}
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Dashboard
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Team
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Projects
+              </a>
+            </li>
+          </ul>
+          {/* Left links */}
+        </div>
+        {/* Collapsible wrapper */}
+
+        {/* Right elements */}
+        <div className="d-flex align-items-center">
+          {/* Icon */}
+          <a className="link-secondary me-3" href="#">
+            <i className="fas fa-shopping-cart"></i>
+          </a>
+
+          {/* Notifications */}
+          <div className="dropdown">
+            <a
+              data-mdb-dropdown-init
+              className="link-secondary me-3 dropdown-toggle hidden-arrow"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              aria-expanded="false"
+            >
+              <i className="fas fa-bell"></i>
+              <span className="badge rounded-pill badge-notification bg-danger">1</span>
+            </a>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  Some news
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Another news
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* Avatar */}
+          <div className="dropdown">
+            <a
+              data-mdb-dropdown-init
+              className="dropdown-toggle d-flex align-items-center hidden-arrow"
+              href="#"
+              id="navbarDropdownMenuAvatar"
+              role="button"
+              aria-expanded="false"
+            >
+              <img
+                src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                className="rounded-circle"
+                height="25"
+                alt="Black and White Portrait of a Man"
+                loading="lazy"
+              />
+            </a>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="navbarDropdownMenuAvatar"
+            >
+              <li>
+                <a className="dropdown-item" href="#">
+                  My profile
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Settings
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* Right elements */}
+      </div>
+      {/* Container wrapper */}
+    </nav>
+  );
+};
+
+export default Navbar;
+
