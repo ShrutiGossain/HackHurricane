5e7e56bee19186d0edc88bc719e155c1538350a1
vaishali <vaishalirdj14@gmail.com>
2024-01-04 20:04:14 +0530

    modified

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Task = () => {
   return (
-    <div>Task</div>
+    <div></div>
   )
 }
 
--- client/src/pages/UserDashboard.js
+++ client/src/pages/UserDashboard.js
@@ -1,13 +1,25 @@
 import React from "react";
 import Github from "../components/GitHub/Github";
 import Leetcode from "../components/Leetcode/Leetcode";
-import Quotes from "../components/Quotes/Quotes"
+import Quotes from "../components/Quotes/Quotes";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Links from "../components/Links";
+import Task from "../components/Task";
+import Timer from "../components/Timer";
 
 const UserDashboard = () => {
   return (
     <>
+      <Navbar/>
+      {/* <Sidebar/> */}
+      <About/>
       <Github />
       <Leetcode />
+      <Links/>
+      <Timer/>
+      <Task/>
       <Quotes />
     </>
   );
